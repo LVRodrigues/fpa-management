77ebe27f8ee22d50e3107c742948ce12ab25e7de
LVRodrigues <lvrodriguesline@gmail.com>
2023-11-27 07:59:17 -0300

    Header exists.

--- fpa-server/src/auth.rs
+++ fpa-server/src/auth.rs
@@ -1,20 +1,23 @@
-use error::{Error, Result};
-
-use crate::error;
+use crate::error::{Error, Result};
 
 use axum::{http::{Request, header}, middleware::Next, response::Response};
+
+const BEARER: &str = "Bearer ";
 
 pub async fn require<B>(request: Request<B>, next: Next<B>) -> Result<Response> {
     let token = request.headers()
         .get(header::AUTHORIZATION)
         .and_then(|header| header.to_str().ok())
         .and_then(|value| {
-            if value.starts_with("Bearer ") {
-                Some(value[7..].to_owned())
+            if value.starts_with(BEARER) {
+                Some(value[BEARER.len()..].to_owned())
             } else {
                 None
             }
         });
-    println!("==> {:<12} Token: {:?}", "AUTH", token);
+    let token = match token {
+        Some(v) => v,
+        None => return Err(Error::Unauthorized),
+    };
     Ok(next.run(request).await)
 }
