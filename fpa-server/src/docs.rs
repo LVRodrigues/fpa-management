f2916d63dc821b09aeeb4ca1ce8f703c6095a9f9
LVRodrigues <lvrodriguesline@gmail.com>
2024-02-08 16:56:07 -0300

    .

--- fpa-server/src/docs.rs
+++ fpa-server/src/docs.rs
@@ -1,36 +1,6 @@
 use serde_derive::Serialize;
 use utoipa::{openapi::{security::{Flow, OAuth2, Password, Scopes, SecurityScheme}, Components}, Modify, OpenApi, ToSchema};
 
-/* #region Arrays */
-
-///
-/// A redeclaração da estrutura Project é apenas para renomear o tipo na montagem da Matriz Projects.
-/// O tipo válido para operação no sistem está em model::projects::Model, que é renomeado
-/// para Project na documentação da API.
-/// 
-/// No módulo model estão os tipos corretos gerados pelo cliente sea-orm-cli
-/// 
-
-#[derive(Debug, Serialize)]
-struct Project;
-
-#[derive(Debug, Serialize, ToSchema)]
-struct Projects(Vec<Project>);
-
-#[derive(Debug, Serialize)]
-struct User;
-
-#[derive(Debug, Serialize, ToSchema)]
-struct Users(Vec<User>);
-
-///
-/// Ficou, por enquanto, como curiosidade. 
-/// 
-/// Use o objeto Page para recuperar listas.
-/// 
-
-/* #endregion */
-
 #[derive(OpenApi)]
 #[openapi(
     tags(
@@ -44,9 +14,7 @@ struct Users(Vec<User>);
     components(schemas(
         crate::model::page::Page,
         crate::model::users::Model,
-        Users,
         crate::model::projects::Model,
-        Projects
     )),
     modifiers(&SecuritySchemas),
 )]
