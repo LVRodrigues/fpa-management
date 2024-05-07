70d9215477cc42a272b1fc175a8052b4d596efda
LVRodrigues <lvrodriguesline@gmail.com>
2024-05-07 10:54:17 -0300

    Tests

--- fpa-server/tests/projects.rs
+++ fpa-server/tests/projects.rs
@@ -1,5 +1,3 @@
-use std::sync::{Arc, Mutex, OnceLock};
-
 use anyhow::Result;
 use reqwest::StatusCode;
 use uuid::Uuid;
@@ -8,18 +6,20 @@ use serde_json::json;
 
 mod tokens;
 
+const USERNAME: &str = "user";
+const PASSWORD: &str = "fpa-pass";
+
 const PROJECT_NAME: &str = "Running-Test";
 
 #[derive(Debug)]
 struct Data {
-    project: Option<Uuid>,
+    project: Uuid,
 }
 impl Data {
-    fn new() -> Self {
-        Self { project: None }
+    fn new(id: Uuid) -> Self {
+        Self { project: id }
     }
 }
-static TEST: OnceLock<Arc<Mutex<Data>>> = OnceLock::new();
 
 async fn list(token: &String) -> Result<()> {
     let response = reqwest::Client::new()
@@ -30,18 +30,16 @@ async fn list(token: &String) -> Result<()> {
     assert_eq!(response.status(), StatusCode::OK);
 
     let json = response.json::<serde_json::Value>().await?;
-    assert_eq!(*json.get("total").unwrap(), json!(10));
-    assert_eq!(*json.get("index").unwrap(), json!(1));
-    assert_eq!(*json.get("size").unwrap(), json!(10));
-    assert_eq!(*json.get("records").unwrap(), json!(100));
-    assert!(json.get("items").unwrap().is_array());
-    assert_eq!(json.get("items").unwrap().as_array().unwrap().len(), 10);
+    assert_eq!(json["pages"], json!(10));
+    assert_eq!(json["index"], json!(1));
+    assert_eq!(json["size"], json!(10));
+    assert_eq!(json["records"], json!(100));    
+    assert!(json["items"].is_array());
+    assert_eq!(json["items"].as_array().unwrap().len(), 10);
     Ok(())
 }
 
-async fn create(token: &String) -> Result<()> {
-    let data = Arc::new(Mutex::new(Data::new()));
-
+async fn create(token: &String) -> Result<Data> {
     let response = reqwest::Client::new()
         .post(format!("http://localhost:5000/api/projects?name={}", PROJECT_NAME))
         .bearer_auth(token)
@@ -52,48 +50,62 @@ async fn create(token: &String) -> Result<()> {
 
     let json = response.json::<serde_json::Value>().await?;
     assert!(json.get("project").is_some());
-    assert_eq!(*json.get("name").unwrap(), PROJECT_NAME);
+    assert_eq!(json["name"].as_str().unwrap(), PROJECT_NAME);
     assert!(json.get("time").is_some());
     assert!(json.get("user").is_some());
 
-    data.lock().unwrap().project = Some(Uuid::parse_str(json["project"].as_str().unwrap())?);
-    TEST.set(data).unwrap();
-    assert!(TEST.get().unwrap().lock().unwrap().project.is_some());
+    let id = Uuid::parse_str(json["project"].as_str().unwrap())?;
+    let data = Data::new(id);
+    assert!(!data.project.is_nil());
 
-    Ok(())
+    Ok(data)
 }
 
-async fn find_by_id(token: &String) -> Result<()> { 
-    let data = match TEST.get() {
-        Some(v) => v,
-        None => panic!("Run create() test first.")
-    };
-    let data = data.lock().unwrap();
-    assert!(data.project.is_some());
-
+async fn find_by_id(token: &String, id: &Uuid) -> Result<()> { 
     let response = reqwest::Client::new()
-    .get(format!("http://localhost:5000/api/projects/{}", data.project.unwrap()))
-    .bearer_auth(token)
-    .send()
-    .await?;
+        .get(format!("http://localhost:5000/api/projects/{}", id))
+        .bearer_auth(token)
+        .send()
+        .await?;
     assert_eq!(response.status(), StatusCode::OK);
 
     let json = response.json::<serde_json::Value>().await?;
-    assert_eq!(json["project"].as_str().unwrap(), data.project.unwrap().to_string().as_str());
+    assert_eq!(json["project"].as_str().unwrap(), id.to_string().as_str());
     assert_eq!(json["name"].as_str().unwrap(), PROJECT_NAME);
 
     Ok(())
 }
 
+async fn find_by_name(token: &String) -> Result<()> { 
+    let response = reqwest::Client::new()
+        .get(format!("http://localhost:5000/api/projects?name={}", PROJECT_NAME))
+        .bearer_auth(token)
+        .send()
+        .await?;
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let json = response.json::<serde_json::Value>().await?;
+    assert_eq!(json["pages"], json!(1));
+    assert_eq!(json["index"], json!(1));
+    assert_eq!(json["size"], json!(1));
+    assert_eq!(json["records"], json!(1));
+    assert!(json["items"].is_array());
+    assert_eq!(json["items"].as_array().unwrap().len(), 1);
+    assert_eq!(json["items"][0]["name"].as_str().unwrap(), PROJECT_NAME);
+    Ok(())
+}
 
 #[tokio::test]
 async fn execute() -> Result<()> {
-    let token = tokens::request_token("user", "fpa-pass", Tenant::TENANT_DEFAULT).await?;
+    let token = tokens::request_token(USERNAME, PASSWORD, Tenant::TENANT_DEFAULT).await?;
     assert!(!token.is_empty());
 
     list(&token).await?;
-    create(&token).await?;
-    find_by_id(&token).await?;
+
+    let data = create(&token).await?;
+
+    find_by_id(&token, &data.project).await?;
+    find_by_name(&token).await?;
 
     Ok(())
 }
