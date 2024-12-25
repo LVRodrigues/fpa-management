79386229985ae86605e0672c4dea3d9dfd6eda0f
LVRodrigues <lvrodriguesline@gmail.com>
2024-12-25 15:47:41 +0000

    Functions list filtered.

--- fpa-server/tests/functions.rs
+++ fpa-server/tests/functions.rs
@@ -27,6 +27,56 @@ async fn list(token: &String, project: &Uuid, module: &Uuid) -> Result<()> {
     Ok(())
 }
 
+async fn list_by_name(token: &String, project: &Uuid, module: &Uuid) -> Result<()> {
+    let response = reqwest::Client::new()
+        .get(format!("{}/{}/modules/{}/functions?name=ALI", URL, project, module))
+        .bearer_auth(token)
+        .send()
+        .await?;
+    assert_eq!(response.status(), StatusCode::OK); 
+
+    let json = response.json::<serde_json::Value>().await?;
+
+    assert_eq!(json["pages"], json!(1));
+    assert_eq!(json["index"], json!(1));
+    assert_eq!(json["size"], json!(1));
+    assert_eq!(json["records"], json!(1));    
+    assert!(json["items"].is_array());
+    assert_eq!(json["items"].as_array().unwrap().len(), 1);
+
+    let items = json["items"].as_array().unwrap();
+    assert!(items[0].get("ALI").is_some());
+    let value = items[0].get("ALI").unwrap();
+    assert!(value["name"].as_str().unwrap().contains("ALI"));
+
+    Ok(())
+}
+
+async fn list_by_type(token: &String, project: &Uuid, module: &Uuid) -> Result<()> {
+    let response = reqwest::Client::new()
+        .get(format!("{}/{}/modules/{}/functions?type=EE", URL, project, module))
+        .bearer_auth(token)
+        .send()
+        .await?;
+    assert_eq!(response.status(), StatusCode::OK); 
+
+    let json = response.json::<serde_json::Value>().await?;
+
+    assert_eq!(json["pages"], json!(1));
+    assert_eq!(json["index"], json!(1));
+    assert_eq!(json["size"], json!(1));
+    assert_eq!(json["records"], json!(1));    
+    assert!(json["items"].is_array());
+    assert_eq!(json["items"].as_array().unwrap().len(), 1);
+    
+    let items = json["items"].as_array().unwrap();
+    assert!(items[0].get("EE").is_some());
+    let value = items[0].get("EE").unwrap();
+    assert!(value["name"].as_str().unwrap().contains("EE"));
+
+    Ok(())
+}
+
 #[tokio::test]
 async fn execute() -> Result<()> {
     let token = tokens::request_token(USERNAME, PASSWORD, Tenant::TENANT_DEFAULT).await?;
@@ -36,6 +86,11 @@ async fn execute() -> Result<()> {
     let module = selects::module(&token, &project).await?;
 
     list(&token, &project, &module).await?;
+
+    list_by_name(&token, &project, &module).await?;
+
+    list_by_type(&token, &project, &module).await?;
     
     Ok(())
 }
+
