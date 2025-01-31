f34dde4e790f7758bccbf3c9acd25c3784a808d0
LVRodrigues <lvrodriguesline@gmail.com>
2025-01-31 10:53:02 +0000

    Format release date in footer and import CommonModule for improved functionality

--- fpa-client/src/app/footer/footer.component.ts
+++ fpa-client/src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-footer',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
