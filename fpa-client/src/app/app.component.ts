77bb1b1218b76151bd6f71d266082043f82cf7dc
LVRodrigues <lvrodriguesline@gmail.com>
2025-01-31 01:17:32 +0000

    Add localization support for Portuguese; update Angular configuration and app title

--- fpa-client/src/app/app.component.ts
+++ fpa-client/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { FooterComponent } from "./footer/footer.component";
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-    title = 'FPA Calculator';
+    title = $localize`:Application title:FPA Calculator`;
 }
