97f34468b6d2451a552b632814ebbcd117f22eac
LVRodrigues <lvrodriguesline@gmail.com>
2025-02-01 20:00:12 +0000

    Add module type declarations and enhance global styles for improved layout

--- fpa-client/release.ts
+++ fpa-client/release.ts
@@ -1,28 +1,28 @@
-import {replaceInFileSync} from 'replace-in-file'
+import { replaceInFileSync } from 'replace-in-file'
 import moment from 'moment-timezone';
 
 const stamp: string = moment.tz('America/Sao_Paulo').format();
 const options = {
-    files: [
-        'src/environments/environment.ts',
-        'src/environments/environment.development.ts',
-    ],
-    from: /releaseStamp: '(.*)'/g,
-    to: "releaseStamp: '" + stamp + "'",
-    allowEmptyPaths: false,
+	files: [
+		'src/environments/environment.ts',
+		'src/environments/environment.development.ts',
+	],
+	from: /releaseStamp: '(.*)'/g,
+	to: "releaseStamp: '" + stamp + "'",
+	allowEmptyPaths: false,
 };
 
 try {
-    const files = replaceInFileSync(options);
-    if (files.length === 0) {
-        throw new Error(
-            'Tenha certeza que o arquivo "' +
-            options.files +
-            '" possua a propriedade "releaseStamp: "'
-        );
-    }
-    console.log('Data de distribuição: ' + stamp);
+	const files = replaceInFileSync(options);
+	if (files.length === 0) {
+		throw new Error(
+			'Tenha certeza que o arquivo "' +
+			options.files +
+			'" possua a propriedade "releaseStamp: "'
+		);
+	}
+	console.log('Data de distribuição: ' + stamp);
 } catch (error) {
-    console.error("Erro:", error);
-    throw error;
+	console.error("Erro:", error);
+	throw error;
 }
