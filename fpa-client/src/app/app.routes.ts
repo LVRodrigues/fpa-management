16eca38bb452b073fb66cc02ca9b0f68f51d7fec
LVRodrigues <lvrodriguesline@gmail.com>
2025-02-13 19:34:04 +0000

    Remove login route and auth guard from routing configuration

--- fpa-client/src/app/app.routes.ts
+++ fpa-client/src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './guards/auth.guard';
-import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', canActivate: [ authGuard ], component: HomeComponent },
-    { path: 'login', component: LoginComponent },
+    { path: 'home', canActivate: [  ], component: HomeComponent },
     { path: '**', redirectTo: 'home'}
 ];
