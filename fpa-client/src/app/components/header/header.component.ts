2b3a2753cc511c37cfb6e377d41715c4183e2dda
LVRodrigues <lvrodriguesline@gmail.com>
2025-02-14 16:19:00 +0000

    Enhance header component to display username and sign-out functionality

--- fpa-client/src/app/components/header/header.component.ts
+++ fpa-client/src/app/components/header/header.component.ts
@@ -1,20 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 import { AppComponent } from '../../app.component';
+import Keycloak from 'keycloak-js';
 
 @Component({
 	selector: 'app-header',
-	imports: [MatToolbarModule],
+	imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDividerModule],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
 
-	title: String = "";
+	username: String = "";
+	title: String | undefined = "";
 
-	constructor(private app: AppComponent) {
+	constructor(
+		private app: AppComponent,
+		private keycloak: Keycloak
+	) {
 		this.title = app.title;
-	 }
+		this.username = keycloak.tokenParsed?.['name'];
+	}
 
+	signout(): void {
+		this.keycloak.logout();
+	}
 }
